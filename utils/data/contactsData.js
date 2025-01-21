35f3222e87c5bacd2eaa2a83d84797a166f2e91d
Kundan Kumar <kundan.kumar.b11@gmail.com>
2025-01-21 20:44:10 +0530

    dev portfolio

---
+++ utils/data/contactsData.js
@@ -0,0 +1,12 @@
+export const contactsData = {
+    email: "kundan.kumar.b11@gmail.com",
+    phone: "+91 6200886475",
+    address: "New Delhi, India",
+    github: "https://github.com/Nukealbert",
+    facebook: '',
+    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    twitter: '',
+    stackOverflow: '',
+    devUsername: "Kundan Kumar",
+}
+
---
+++ utils/data/personal-data.js
@@ -0,0 +1,17 @@
+export const personalData = {
+  name: "Kundan Kumar",
+  profile: "/profile.jpeg",
+  designation: "Full-Stack Software Developer",
+  description: "My name is Kundan Kumar....",
+  email: "kundan.kumar.b11@gmail.com",
+  phone: "+91 6200886475",
+  address: "New Delhi, India",
+  github: "https://github.com/Nukealbert",
+  facebook: "",
+  linkedIn: "https://www.linkedin.com/in/developerkundankumar/",
+  twitter: "https://twitter.com/said7388",
+   stackOverflow: "",
+   leetcode: "",
+   devUsername: "",
+  resume: "...",
+};
